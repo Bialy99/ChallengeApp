e9a6b43f10fb39a5a29dedbe33254989b5b2a5c3
michal.lesiecki <michal.lesiecki@student.put.poznan.pl>
2023-02-14 01:12:02 +0100

    Zad dom dzień 4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,18 @@
+﻿//zadanie domowe 4 dzień
+string name = "Ewa";
+bool female = true;
+int age = 33;
+
+if (female && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
